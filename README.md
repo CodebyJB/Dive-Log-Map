aa19781899f4632cd280e2b18410bd21157b072b
Jennifer <106908910+JenniferBraunbarth@users.noreply.github.com>
2023-05-15 10:09:15 +0200

    second commit

--- README.md
+++ README.md
@@ -2,20 +2,21 @@
 
 ## Description:
 
-
 ## Technologies Used:
-* HTML
-* CSS
-* JavaScript
-* Bootstrap
+
+- HTML
+- CSS
+- JavaScript
+- Bootstrap
 
 ## Features:
-* responsive design
+
+- responsive design
 
 ## Demo:
-View project demo at 
+
+View project demo at https://jb-dive-log-map.netlify.app/
 
 ## Contributing:
+
 If you would like to contribute, please fork the repository and use a feature branch. Pull requests are warmly welcome.
-
-
--- logic.js
+++ logic.js
@@ -1,1 +1,11 @@
-'use strict';
+"use strict";
+
+const form = document.querySelector(".form");
+const containerDives = document.querySelector(".dives");
+const inputName = document.querySelector(".form_input_name");
+const inputDepth = document.querySelector(".form_input_depth");
+const inputDuration = document.querySelector(".form_input_duration");
+const inputWaves = document.querySelector(".form_input_waves");
+const inputWeather = document.querySelector(".form_input_weather");
+const inputHighlight = document.querySelector(".form_input_highlight");
+const formBtn = document.querySelector(".form_btn");
